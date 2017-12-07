98d67b3166bb9d70c7facab571214437b0569088
xosherry <xuewenli@umail.ucsb.edu>
2017-12-06 21:12:55 -0800

    reverting change and adding db code

--- app/src/main/java/edu/ucsb/cs/cs184/georgelieu/thingamajob/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs184/georgelieu/thingamajob/MainActivity.java
@@ -3,8 +3,11 @@ package edu.ucsb.cs.cs184.georgelieu.thingamajob;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
-//    DatabaseReference mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 //    mDatabaseReference.child();
 
 
