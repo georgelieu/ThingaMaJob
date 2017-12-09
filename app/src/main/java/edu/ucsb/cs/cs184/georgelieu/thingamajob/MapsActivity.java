cac497750966b15435c2490455de81a94f76f630
Thien Hoang <thienhoang@umail.ucsb.edu>
2017-12-09 00:37:10 -0800

    Maps
    * Got Map to show up and Pass Auth
    * Localize Map to IV
    * Get rid of Sign out button on Map

--- app/src/main/java/edu/ucsb/cs/cs184/georgelieu/thingamajob/MapsActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs184/georgelieu/thingamajob/MapsActivity.java
@@ -5,12 +5,9 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -40,11 +37,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         Button signOut = (Button) findViewById(R.id.sign_out_button);
         signOut.setOnClickListener(new View.OnClickListener() {
