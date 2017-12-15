d53ec7b3fa09f4fc08acdaf11e298fedd39ac7c7
Simon Wong <thatonesimon@gmail.com>
2017-12-15 10:02:54 -0800

    Fixing some smol bugs

--- app/src/main/java/edu/ucsb/cs/cs184/georgelieu/thingamajob/DatabaseHelper.java
+++ app/src/main/java/edu/ucsb/cs/cs184/georgelieu/thingamajob/DatabaseHelper.java
@@ -131,8 +131,6 @@ public class DatabaseHelper {
 
     public static void taskTaken(String id) {
         DatabaseReference task = mTaskCloudEndPoint.child(id);
-        // TODO: Simon, we commented out the below code. did u mean to put doer instead of original poster's email?
-        //task.child("original_poster_email").setValue(MainActivity.current_user_email);
         task.child("task_doer_email").setValue(MainActivity.current_user_email);
         task.child("isFinished").setValue(true);
     }
